f100a7d665e2e838d9744f38338f0025383fe1a1
Pedro Dias Junior <153040441+juniorpdiasCPLV@users.noreply.github.com>
2023-12-11 22:16:58 -0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+function carregarimagem(){
+
+
+    <li><a href ="foto.html">Catalogo</a></li>;
+
+/*    
+    document.getElementById("imagem").innerHTML = "<  img src = 'images/IMG_4315.jpg' width = '700'>";
+    
+    var p = document.getElementById('foto.html');
+    p.innerHTML = "foto.html";
+
+    const win = window.open(  <li><a href ="foto.html">Catalogo</a></li>,'_blank');
+    win.focus();
+
+    
+    
+
+    document.getElementById("imagem").innerHTML = "<img src = 'images/IMG_4315.jpg' width = '700'>";
+
+    document.getElementById("imagem").innerHTML = document.getElementById("imagem").innerHTML + "<img src = 'images/IMG_4316.jpg' width = '700'>";
+    document.getElementById("imagem").innerHTML = document.getElementById("imagem").innerHTML + "<img src = 'images/IMG_4317.jpg' width = '700'>";
+ 
+    const win = window.open( <li><a href ="foto.html">Catalogo</a></li>,'_blank');
+    win.focus();
+ */ 
+
+/*
+
+newFunction();
+
+
+
+    function newFunction() {
+        const win = window.open('D:\UNIBTA\Projeto-Garagem-BadCondition-Catalago\images\IMG_4314.jpg', '_blank');
+        win.focus();
+    }
+ */   
+}
